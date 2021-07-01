574936f518438ec0b1e337325319b6b8eccc3cfa
Pedro Brañez <pedrobran8@gmail.com>
2021-07-01 09:32:43 -0400

    Adding some styles to Appbar

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -17,12 +17,23 @@
       <v-col offset="6" cols="2">
         <v-menu offset-y>
           <template v-slot:activator="{ on, attrs }">
-            <v-btn height="40" rounded color="primary" v-bind="attrs" v-on="on">
-              <v-icon left color="charcoal0">mdi-account-circle</v-icon>Juan
-              Perez
+            <v-btn
+              class="btn-user"
+              height="40"
+              rounded
+              color="primary"
+              v-bind="attrs"
+              v-on="on"
+              v-on:click="changeIcon"
+            >
+              <v-icon left color="charcoal0" size="28px" class="ml-1"
+                >mdi-account-circle</v-icon
+              >
+              <span class="ml-1">{{ user }}</span>
+              <v-icon right color="charcoal0">{{ arrowIcon }}</v-icon>
             </v-btn>
           </template>
-          <v-list>
+          <v-list color="primary" dark>
             <v-list-item v-for="(item, index) in items" :key="index">
               <v-list-item-title>{{ item.title }}</v-list-item-title>
             </v-list-item>
@@ -30,8 +41,6 @@
         </v-menu>
       </v-col>
     </v-row>
-
-    <div class="text-center"></div>
   </v-app-bar>
 </template>
 
@@ -45,7 +54,18 @@ export default {
       { title: "Sube a premiun" },
       { title: "Cerrar sesión" },
     ],
+    user: "Juan Perez",
+    arrowIcon: "mdi-arrow-down-drop-circle-outline",
   }),
+  methods: {
+    changeIcon() {
+      if (this.arrowIcon == "mdi-arrow-down-drop-circle-outline") {
+        this.arrowIcon = "mdi-arrow-up-drop-circle-outline";
+      } else {
+        this.arrowIcon = "mdi-arrow-down-drop-circle-outline";
+      }
+    },
+  },
 };
 </script>
 <style>
@@ -55,5 +75,8 @@ export default {
 }
 .v-input__prepend-inner {
   align-self: center !important;
+}
+.btn-user {
+  text-transform: none !important;
 }
 </style>
